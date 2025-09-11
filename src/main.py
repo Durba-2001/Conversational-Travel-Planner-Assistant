85923985e40b64c46686da0c96ccc11ecf6ff0f3
Durba_2001 <durba028@gmail.com>
2025-09-11 20:14:00 +0530

    modified main file

--- src/main.py
+++ src/main.py
@@ -3,6 +3,5 @@ from src.api.router import router as api_router
 from src.auth.router import router as auth_router
 
 app = FastAPI(title="Travel Planner Assistant",version="1.0",description="conversation travel assistant")
-
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 app.include_router(api_router, prefix="/api", tags=["Chat"])
